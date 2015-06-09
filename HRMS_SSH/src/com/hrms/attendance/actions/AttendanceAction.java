acd983c8379c5a4d84ef7cf4fc768fa0d1bae312
yummy222 <616119266@qq.com>
2015-06-09 17:03:26 +0800

    添加按日期查询个人考勤功能，杨明杰，2015-06-09

--- HRMS_SSH/src/com/hrms/attendance/actions/AttendanceAction.java
+++ HRMS_SSH/src/com/hrms/attendance/actions/AttendanceAction.java
@@ -12,6 +12,7 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.sun.xml.internal.fastinfoset.sax.Properties;
 
 public class AttendanceAction extends ActionSupport {
+	private String Date;
 	private Attendance attendance;
 	@Resource
 	private AttendanceService attendanceservice;
@@ -26,6 +27,26 @@ public class AttendanceAction extends ActionSupport {
 		
 	}
 	
+	public String workerAttendanceSearchByDate(){
+		//System.out.println(Date);
+		String hql = "from Attendance where attendanceWorkerOid = 1 and attendanceDate = '"+Date+"' order by attendanceDate desc";
+		List<Attendance> attendancelistdate = attendanceservice.searchAttendance(hql);
+		
+		Map session = ActionContext.getContext().getSession();
+		session.put("attendancelistdate", attendancelistdate);
+		ActionContext.getContext().setSession(session);
+		
+		return SUCCESS;
+	}
+	
+	public String getDate() {
+		return Date;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
 	public Attendance getAttendance() {
 		return attendance;
 	}
