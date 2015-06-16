7799d1cdfc2ef5ac6109434a5c9bff9539ca0e08
mrluuuuu <milktea@vip.qq.com>
2015-06-16 10:56:50 +0800

    重写数据库培训部分，新增3个表（Cultivation、CultivationSign、CultivationPerson）。更新SQL语句，更新POJO类。

--- HRMS_SSH/src/com/hrms/pojo/Department.java
+++ HRMS_SSH/src/com/hrms/pojo/Department.java
@@ -3,91 +3,102 @@ package com.hrms.pojo;
 import java.util.HashSet;
 import java.util.Set;
 
+
 /**
  * Department entity. @author MyEclipse Persistence Tools
  */
 
-public class Department implements java.io.Serializable {
+public class Department  implements java.io.Serializable {
 
-	// Fields
 
-	private Integer departmentOid;
-	private Department department;
-	private String departmentNo;
-	private String departmentName;
-	private Set workers = new HashSet(0);
-	private Set departments = new HashSet(0);
+    // Fields    
 
-	// Constructors
+     private Integer departmentOid;
+     private Department department;
+     private String departmentNo;
+     private String departmentName;
+     private Set workers = new HashSet(0);
+     private Set departments = new HashSet(0);
 
-	/** default constructor */
-	public Department() {
-	}
+
+    // Constructors
+
+    /** default constructor */
+    public Department() {
+    }
 
 	/** minimal constructor */
-	public Department(String departmentNo, String departmentName) {
-		this.departmentNo = departmentNo;
-		this.departmentName = departmentName;
-	}
+    public Department(String departmentNo, String departmentName) {
+        this.departmentNo = departmentNo;
+        this.departmentName = departmentName;
+    }
+    
+    /** full constructor */
+    public Department(Department department, String departmentNo, String departmentName, Set workers, Set departments) {
+        this.department = department;
+        this.departmentNo = departmentNo;
+        this.departmentName = departmentName;
+        this.workers = workers;
+        this.departments = departments;
+    }
 
-	/** full constructor */
-	public Department(Department department, String departmentNo,
-			String departmentName, Set workers, Set departments) {
-		this.department = department;
-		this.departmentNo = departmentNo;
-		this.departmentName = departmentName;
-		this.workers = workers;
-		this.departments = departments;
-	}
+   
+    // Property accessors
 
-	// Property accessors
+    public Integer getDepartmentOid() {
+        return this.departmentOid;
+    }
+    
+    public void setDepartmentOid(Integer departmentOid) {
+        this.departmentOid = departmentOid;
+    }
 
-	public Integer getDepartmentOid() {
-		return this.departmentOid;
-	}
+    public Department getDepartment() {
+        return this.department;
+    }
+    
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 
-	public void setDepartmentOid(Integer departmentOid) {
-		this.departmentOid = departmentOid;
-	}
+    public String getDepartmentNo() {
+        return this.departmentNo;
+    }
+    
+    public void setDepartmentNo(String departmentNo) {
+        this.departmentNo = departmentNo;
+    }
 
-	public Department getDepartment() {
-		return this.department;
-	}
+    public String getDepartmentName() {
+        return this.departmentName;
+    }
+    
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
+    public Set getWorkers() {
+        return this.workers;
+    }
+    
+    public void setWorkers(Set workers) {
+        this.workers = workers;
+    }
 
-	public String getDepartmentNo() {
-		return this.departmentNo;
-	}
+    public Set getDepartments() {
+        return this.departments;
+    }
+    
+    public void setDepartments(Set departments) {
+        this.departments = departments;
+    }
+   
 
-	public void setDepartmentNo(String departmentNo) {
-		this.departmentNo = departmentNo;
-	}
 
-	public String getDepartmentName() {
-		return this.departmentName;
-	}
 
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
 
-	public Set getWorkers() {
-		return this.workers;
-	}
 
-	public void setWorkers(Set workers) {
-		this.workers = workers;
-	}
 
-	public Set getDepartments() {
-		return this.departments;
-	}
 
-	public void setDepartments(Set departments) {
-		this.departments = departments;
-	}
 
 }
